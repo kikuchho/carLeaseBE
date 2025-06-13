60b2718e882c2342e1f6c739121c049d71f78b66
kikuccho <yutotachekawa@gmail.com>
2025-06-13 18:46:43 +0900

    first push (backend)

---
+++ clbackend/api/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers 
+
+class UserSerializer(serializer.ModelSerializer): 
+    class Meta: 
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {
+            "password": {
+                "write_only": True
+            }
+        }
+
+    def craete(self, validated_data):
+        user = User.objects.craete_user(**validated_data)
+        return user
+    
+    
---
+++ readme.txt
@@ -0,0 +1,12 @@
+set up venv : 
+1 : python3 -m venv myvenv      
+2 : myvenv\Scripts\activate
+
+start a project : 
+1: cd to clbackend 
+2: make sure you are in venv and type python maange.py starapp api <- this create api folder
+
+
+django use so called ORM (object relational mapping )
+views handles sql 
+urls handle interaction between frontend and backend
